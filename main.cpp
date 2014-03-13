472a45bf67322639b752cd189ab0cd0ce8b14e6f
maratkanov <alexwitcher@gmail.com>
2014-03-13 11:39:34 +0400

    one client then shutdown

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
 
 using boost::asio::ip::tcp;
 
@@ -11,22 +12,38 @@ std::string make_datetime_string() {
     return ctime(&now);
 }
 
-int main() {
-    try {
-        boost::asio::io_service io_service;
-        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 8080));
+typedef boost::shared_ptr<tcp::socket> socket_ptr;
 
-        for (;;) {
-            tcp::socket socket(io_service);
-            acceptor.accept(socket);
-
+void client_session(socket_ptr socket) {
+    while (true) {
+        char data[512];
+        size_t len = socket->read_some(boost::asio::buffer(data));
+        if (len > 0) {
+            std::cout << data;
             std::string message = make_datetime_string();
             boost::system::error_code ignored_error;
-            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+
+            boost::asio::write(*socket, boost::asio::buffer(message), ignored_error);
         }
     }
-    catch (std::exception& e) {
-        std::cerr << e.what() << std::endl;
+}
+
+int main() {
+    boost::asio::io_service io_service;
+    tcp::resolver resolver(io_service);
+
+    std::string address = "0.0.0.0";
+    std::string port = "8080";
+
+    tcp::resolver::query query(address, port);
+    tcp::endpoint endpoint = *resolver.resolve(query);
+
+    tcp::acceptor acceptor(io_service, endpoint);
+
+    while (true) {
+        socket_ptr sock(new tcp::socket(io_service));
+        acceptor.accept(*sock);
+        boost::thread(boost::bind(client_session, sock));
     }
 
     return 0;
